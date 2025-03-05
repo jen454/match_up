7e4466ec40d1030983ebabf3cacd67abb3850c32
Jinwook <jinwook2765@naver.com>
2025-03-05 22:03:44 +0900

    style: 버튼명 수정

--- src/app/participants/[id]/page.tsx
+++ src/app/participants/[id]/page.tsx
@@ -14,25 +14,19 @@ const ParticipantsPage = () => {
   const params = useParams();
   const numParticipants = Number(params.id);
 
-  if (!numParticipants) {
-    return <div className="flex items-center justify-center">Loading...</div>; // id가 없으면 로딩 메시지 표시
-  }
-
   // 모든 참가자의 정보를 관리하는 상태
   const [players, setPlayers] = useState<Player[]>(
-      Array.from({ length: numParticipants }, (_, index) => ({
-        id: index + 1,
-        name: "",
-        handicap: 0,
-      }))
+    Array.from({ length: numParticipants }, (_, index) => ({
+      id: index + 1,
+      name: "",
+      handicap: 0,
+    })),
   );
 
   // 개별 참가자 정보 업데이트 함수
   const updatePlayer = (id: number, updatedData: Partial<Player>) => {
     setPlayers((prev) =>
-        prev.map((player) =>
-            player.id === id ? { ...player, ...updatedData } : player
-        )
+      prev.map((player) => (player.id === id ? { ...player, ...updatedData } : player)),
     );
   };
 
@@ -40,20 +34,20 @@ const ParticipantsPage = () => {
     console.log(players);
   };
 
+  if (!numParticipants) {
+    return <div className="flex items-center justify-center">Loading...</div>; // id가 없으면 로딩 메시지 표시
+  }
+
   return (
     <div className="h-screen flex flex-col items-center">
       <div className="flex flex-col items-center gap-12 mt-32">
         <div className="text-3xl font-bold text-center">게임 참가자 정보 입력</div>
         <div className="flex flex-col gap-6">
           {players.map((player) => (
-              <PlayerCard
-                  key={player.id}
-                  player={player}
-                  updatePlayer={updatePlayer}
-              />
+            <PlayerCard key={player.id} player={player} updatePlayer={updatePlayer} />
           ))}
         </div>
-        <Button text={"대진표 생성하기"} onClick={onClickButton} />
+        <Button text={"게임 생성하기"} onClick={onClickButton} />
       </div>
     </div>
   );
